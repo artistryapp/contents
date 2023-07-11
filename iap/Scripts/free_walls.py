0e2ae1d70f29e5fa9ce30a13801d2f74e69f7c68
YOGESH GOSAVI <135151645+artistryapp@users.noreply.github.com>
2023-07-11 17:44:28 +0530

    Init

---
+++ iap/Scripts/free_walls.py
@@ -0,0 +1,16 @@
+import json
+
+data = []
+
+for i in range(101, 0, -1):
+    entry = {
+        "a": "YoG",
+        "d": "1472×3200",
+        "n": f"name {i}",
+        "t": f"artistry_{i}.jpeg",
+        "u": f"artistry_{i}.jpeg"
+    }
+    data.append(entry)
+
+with open("data.json", "w") as json_file:
+    json.dump(data, json_file, indent=2)
