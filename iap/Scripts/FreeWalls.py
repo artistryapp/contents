540ca710430710ced72e96920a58d78adb808149
YOGESH GOSAVI <135151645+artistryapp@users.noreply.github.com>
2024-02-14 06:30:21 +0530

    .

---
+++ iap/Scripts/FreeWalls.py
@@ -0,0 +1,17 @@
+import json
+
+odyssey_data = []
+
+for i in range(273, 189, -1):
+    entry = {
+      "a": "YoG",
+      "d": "1720 x 3728",
+      "c": "",
+      "n": str(i),
+      "t": "zeel_00{}.jpg".format(i),
+      "u": "zeel_00{}.jpg".format(i)
+    }
+    odyssey_data.append(entry)
+
+with open("FreeWalls.json", "w") as json_file:
+    json.dump(odyssey_data, json_file, indent=2)
---
+++ iap/Scripts/ZeelPlus.py
@@ -0,0 +1,17 @@
+import json
+
+odyssey_data = []
+
+for i in range(51, 22, -1):
+    entry = {
+       "a": "YoG",
+        "d": "1472x3200",
+        "c": "",
+        "n": str(i),
+        "t": "ZeelPlus_00{}.jpg".format(i),
+        "u": "ZeelPlus_00{}.jpg".format(i)
+    }
+    odyssey_data.append(entry)
+
+with open("zeelplus.json", "w") as json_file:
+    json.dump(odyssey_data, json_file, indent=2)
