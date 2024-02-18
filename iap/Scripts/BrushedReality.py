c4757e51be072cd46e5739dda4490d35ddbc35ef
YOGESH GOSAVI <135151645+artistryapp@users.noreply.github.com>
2024-02-18 19:08:17 +0530

    .

---
+++ iap/Scripts/BrushedReality.py
@@ -0,0 +1,16 @@
+import json
+
+odyssey_data = []
+
+for i in range(1, 107):
+    entry = {
+       "a": "YoG",
+        "d": "1472x3200",
+        "n": str(i),
+        "t": "/thumbs/brushedReality_00{}.jpg".format(i),
+        "u": "brushedReality_00{}.jpg".format(i)
+    }
+    odyssey_data.append(entry)
+
+with open("brushedReality.json", "w") as json_file:
+    json.dump(odyssey_data, json_file, indent=2)
