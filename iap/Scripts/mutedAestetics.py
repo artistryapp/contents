f55f96e5b715772bc098069ace12bb64f7f89eae
YOGESH GOSAVI <135151645+artistryapp@users.noreply.github.com>
2024-11-25 15:47:30 +0530

    .

---
+++ iap/Scripts/mutedAestetics.py
@@ -0,0 +1,16 @@
+import json
+
+odyssey_data = []
+
+for i in range(1, 66):
+    entry = {
+       "a": "YoG",
+        "d": "1472x3200",
+        "n": str(i),
+        "t": "/thumbs/ma_0{}.jpg".format(i),
+        "u": "ma_0{}.jpg".format(i)
+    }
+    odyssey_data.append(entry)
+
+with open("mutedAesthetics.json", "w") as json_file:
+    json.dump(odyssey_data, json_file, indent=2)
