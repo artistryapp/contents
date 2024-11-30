342c6e999513ea9e93a155d3f82ec0135d4f4374
YOGESH GOSAVI <135151645+artistryapp@users.noreply.github.com>
2024-11-30 15:38:21 +0530

    .

---
+++ iap/Scripts/DeskV1.py
@@ -0,0 +1,16 @@
+import json
+
+odyssey_data = []
+
+for i in range(1, 85):
+    entry = {
+       "a": "YoG",
+        "d": "5824x3264",
+        "n": str(i),
+        "t": "/thumbs/ZeelDesk_{}.jpg".format(i),
+        "u": "ZeelDesk_{}.jpg".format(i)
+    }
+    odyssey_data.append(entry)
+
+with open("ZeelDeskV1.json", "w") as json_file:
+    json.dump(odyssey_data, json_file, indent=2)
